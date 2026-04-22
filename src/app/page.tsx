5c6bb88ccc75cffc583ba8fa0a613f9f71e53aaf
uzzin <01uzzin@gmail.com>
2026-04-22 13:35:26 +0900

    feat: 버튼 컴포넌트 variant & size 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { Info } from "lucide-react";
+
 export default function Home() {
   return (
-    <div>
-      <p className="text-main">Primary</p>
-      <p className="text-sub">Secondary</p>
-      <p className="text-success">Success</p>
-      <p className="text-warning">Warning</p>
-      <p className="text-danger">Danger</p>
-      <br />
-
-      <p className="text-grey1 h1-bold">Text</p>
-      <p className="text-grey2 h1-semibold">Text</p>
-      <p className="text-border p1-bold">Text</p>
-      <p className="text-font-basic p1-medium">Text</p>
-      <p className="text-font-middle p2-semibold">Text</p>
-      <p className="text-font-light p2-regular">Text</p>
-      <br />
-
-      <p className="c1-bold">테스트테스트테스트테스트테스트</p>
-      <p className="c1-medium">TestTestTestTestTest</p>
-      <br />
-
-      <p className="font-bold">한글</p>
-      <p className="font-bold">aaa</p>
-      <br />
-
-      <p className="font-semibold">한글</p>
-      <p className="font-semibold">aaa</p>
-      <br />
-
-      <p className="font-medium">한글</p>
-      <p className="font-medium">aaa</p>
-      <br />
-
-      <p className="font-regular">한글</p>
-      <p className="font-regular">aaa</p>
+    <div className="p-8">
+      <Button variant="btnPurple" size="full">
+        <Info />
+        <span className="p1-semibold">신곡 홍보 링크 만들기</span>
+      </Button>
+      <Button variant="btnWhite" size="full">
+        <Info />
+        <span className="p1-semibold">내 음원 홍보 진단하기</span>
+      </Button>
     </div>
   );
 }
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,6 +19,8 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive/10 text-destructive hover:bg-destructive/20 focus-visible:border-destructive/40 focus-visible:ring-destructive/20 dark:bg-destructive/20 dark:hover:bg-destructive/30 dark:focus-visible:ring-destructive/40",
         link: "text-primary underline-offset-4 hover:underline",
+        btnPurple: "bg-main text-white",
+        btnWhite: "bg-white text-font-middle border-border"
       },
       size: {
         default:
@@ -32,6 +34,7 @@ const buttonVariants = cva(
         "icon-sm":
           "size-7 rounded-[min(var(--radius-md),12px)] in-data-[slot=button-group]:rounded-lg",
         "icon-lg": "size-9",
+        full: "flex gap-[6px] w-full rounded-full py-[18px]"
       },
     },
     defaultVariants: {
