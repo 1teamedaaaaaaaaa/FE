47d7cfdf762f1afd177d6513dfcc4413117680d6
uzzin <01uzzin@gmail.com>
2026-04-22 15:15:37 +0900

    feat: 헤더 & 툴팁 컴포넌트 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,20 @@
 import { Button } from "@/components/ui/button";
-import { Info } from "lucide-react";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
 
 export default function Home() {
   return (
     <div className="p-8">
-      <Button variant="btnPurple" size="full">
-        <Info />
-        <span className="p1-semibold">신곡 홍보 링크 만들기</span>
-      </Button>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button variant="btnPurple" size="full">
+            <span className="p1-semibold">신곡 홍보 링크 만들기</span>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>발매 정보를 입력하면 팬에게 바로<br />공유할 수 있는 링크가 생겨요</p>
+        </TooltipContent>
+      </Tooltip>
       <Button variant="btnWhite" size="full">
-        <Info />
         <span className="p1-semibold">내 음원 홍보 진단하기</span>
       </Button>
     </div>
---
+++ src/components/common/header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div className="flex justify-between items-center h-14">
+      <div className="h3-bold text-main">Peak</div>
+      <div>이미지</div>
+    </div>
+  )
+}
