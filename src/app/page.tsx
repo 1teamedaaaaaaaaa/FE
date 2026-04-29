83b074796db9c26e814adf5dcf1e45e955b90366
배근영 <bgy09270@naver.com>
2026-04-29 10:24:49 +0900

    design: 밤티 이미지 삽입(#28)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { cookies } from "next/headers";
 import HomeButtons from "@/components/home/home-buttons";
+import Image from "next/image";
 
 export default async function Home() {
   const cookieStore = await cookies();
@@ -8,7 +9,9 @@ export default async function Home() {
   return (
     <main className="p-5">
       <div className="mt-25 mb-24 flex flex-col items-center gap-8 text-center">
-        <div className="bg-grey2 h-30 w-30 rounded-full"></div>
+        <div className="bg-grey2 h-30 w-30 rounded-full">
+          <Image src="/bamti-dancing.svg" alt="밤티" width={120} height={120} />
+        </div>
         <div className="flex flex-col gap-6">
           <h1 className="h1-bold">
             신곡 홍보,
