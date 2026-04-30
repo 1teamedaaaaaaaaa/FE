823bbefe730572a8c45ccbef81d96150e8a13587
배근영 <bgy09270@naver.com>
2026-04-30 09:58:25 +0900

    fix: 이미지 파일명 수정 (#59)

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -21,7 +21,7 @@ const SLIDES: Slide[] = [
     titleHighlight: "어떻게 해야 할지 몰라",
     titleAfter: "막막하셨죠?",
     description: "이젠 혼자 하지 마세요.\nPEAK가 도와드릴게요!",
-    src: "/tutorial01.png",
+    src: "/step01.png",
   },
   {
     titleBefore: "신곡을 냈다면",
@@ -29,13 +29,13 @@ const SLIDES: Slide[] = [
     titleAfter: "만들어보세요",
     description:
       "신곡 정보 입력하면 홍보 링크 완성!\n인스타그램 프로필에 딱 붙여두면 돼요.",
-    src: "/tutorial02.png",
+    src: "/step02.png",
   },
   {
     titleBefore: "팬이 왜 안 느는지",
     titleHighlight: "PEAK가 찾아드려요",
     description: "링크와 날짜만 입력하면\n홍보 상태를 점검할 수 있어요",
-    src: "/tutorial03.png",
+    src: "/step03.png",
   },
 ];
 
