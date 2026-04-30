b4ce3a9d205e7ecf637a907edfecf2ab7187b6fa
이유진 <156034560+uzzini@users.noreply.github.com>
2026-04-30 14:11:43 +0900

    #31 feat: 앨범 정보 입력 페이지 기능 연결 (#63)
    * feat: presigned URL 기반 앨범 커버 이미지 S3 업로드 기능 추가 및 동작 확인
    
    * refactor: 스트리밍 코드 유튜브 → 유튜브 뮤직 변경 & 애플 뮤직 아이콘 파일명 변경
    
    * feat: textarea 글자 수 카운트 실시간 업데이트
    
    * design: textarea 스크롤바 흰 배경 제거 및 스타일 조정
    
    * design: 이미지 선택 시 딤 + 아이콘 오버레이
    
    * design: 링크 입력 시 삭제 아이콘 가림 수정 (조건부 패딩 추가)
    
    * feat: 스트리밍 링크 입력 유효성 검사
    
    * feat: 앨범 정보 입력값 유효성 검사 및 에러 상태에 따른 빨간 테두리 적용
    
    * feat: 개발용 JWT 발급 API 연결 & 뮤지션 홍보 생성 기능 추가 및 동작 확인
    
    * design: 앨범 정보 입력 페이지에서 사용하는 alert 모달 디자인 수정
    
    * design: 앨범 정보 입력 페이지 안내 문구 수정
    
    * fix: streamingCode youtube → youtubemusic 변경으로 인한 타입 불일치 수정
    
    * fix: 동일 파일 재업로드 시 onChange 미동작 문제 해결

---
+++ src/types/api-response.ts
@@ -0,0 +1,20 @@
+/******************************
+ * Image Upload
+ ******************************/
+
+// 음악 홍보 이미지 업로드 URL 발급 Res
+export interface GetCoverImgUploadUrlRes {
+  uploadUrl: string;
+  imageKey: string;
+  imageUrl: string;
+}
+
+/******************************
+ * Music Promotion
+ ******************************/
+
+// 뮤지션 홍보 생성 Res
+export interface CreateMusicPromotionRes {
+  trackingUrl: string;
+  promotionId: number;
+}
